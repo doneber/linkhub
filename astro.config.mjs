b97b5bd30c7ed329e61b0bccd57371755b9371ef
Steve Moya Cepeda <100615052@est.uasd.edu.do>
2024-03-07 19:38:22 -0400

    feat: :technologist: Agregado Devtoolbar para tailwind
    Esto nos ayudara a ver los estilos aplicados por tailwind mas facilmente, en desarrollo y directamente en el navegador

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
-
 import tailwind from "@astrojs/tailwind";
+
+import devtoolbarTailwind from "astro-devtoolbar-tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://linkhub.doneber.dev',
-  integrations: [preact(), tailwind()]
+  integrations: [preact(), tailwind(), devtoolbarTailwind()]
 });
