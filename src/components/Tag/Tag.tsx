7712df83734474848218180226f4bf3b4efd7c02
Eber <doneber98@gmail.com>
2024-03-11 18:07:15 -0400

    fix: 🎨 update styles

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -1,7 +1,7 @@
 export const Tag = ({ text }: { text: string }) => {
 	// TODO: Limpiar el "text" o codificarlo para poder pasarlo como query param en la url
 	return (
-		<a href={`/search?q=%23${text}`} className="bg-neutral-800 rounded-xl border-[1px] border-[solid] border-neutral-800 text-sm pt-0.5 pb-1 px-3">
+		<a href={`/search?q=%23${text}`} className="rounded-xl border-[1px] border-[solid] border-neutral-800 bg-neutral-800 px-3 pb-1 pt-0.5 text-sm dark:border-neutral-200 dark:bg-neutral-200 dark:text-neutral-950 dark:hover:bg-neutral-300">
 			<span className="text-base">{text}</span>
 		</a>
 	)
--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "preact/hooks";
 
-
 export default function ToggleTheme() {
   const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "light");
 
@@ -18,6 +17,8 @@ useEffect(() => {
 }, [theme]);
 
 return (
-	<button className="rounded-full border p-1 dark:border-neutral-700" onClick={handleClick}>{theme === "light" ? "🌙" : "🌞"}</button>
+	<button onClick={handleClick}>
+		<svg width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 496" data-astro-cid-oemx5le4=""><path fill="currentColor" d="M8,256C8,393,119,504,256,504S504,393,504,256,393,8,256,8,8,119,8,256ZM256,440V72a184,184,0,0,1,0,368Z" transform="translate(-8 -8)" data-astro-cid-oemx5le4=""></path></svg>
+	</button>
 );
 }
