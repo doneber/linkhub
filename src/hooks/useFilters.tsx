1609a4a069ca10beb13339392b0b4dd6ba4875e2
Eber <doneber98@gmail.com>
2024-03-11 16:12:10 -0400

    feat: ✨ add fusejs for the search

--- src/hooks/useFilters.tsx
+++ src/hooks/useFilters.tsx
@@ -1,18 +1,39 @@
-import type { Resource } from "@src/interfaces/resource.interface";
-import { filtersResources } from "@src/store";
-import { levenshteinDistance } from "@utils/utils.ts";
+import type { Resource } from "@src/interfaces/resource.interface"
+import { filtersResources } from "@src/store"
+import Fuse from "fuse.js"
 
 export function useFilters() {
 	const filters = filtersResources.get()
 	const setFilters = filtersResources.set
 
 	const filterResources = (resources: Resource[]) => {
-		// TODO: improve the searcher algorithm
 		const query = filters.query.toLocaleLowerCase()
-		return resources.filter((resource) => {
-			const title = resource.title.toLocaleLowerCase()
-      return levenshteinDistance(title, query) <= 3
-    })
+
+		// TODO: Probar la mejor configuracion
+		const fuseOptions = {
+			isCaseSensitive: false,
+			// includeScore: false,
+			// shouldSort: true,
+			// includeMatches: false,
+			// findAllMatches: false,
+			// minMatchCharLength: 1,
+			// location: 0,
+			// threshold: 0.6,
+			// distance: 100,
+			// useExtendedSearch: false,
+			ignoreLocation: true,
+			// ignoreFieldNorm: false,
+			// fieldNormWeight: 1,
+			keys: [
+				"title",
+				"description",
+				"hashtags"
+			]
+		}
+		const fuse = new Fuse(resources, fuseOptions)
+
+		const resourcesFound = fuse.search(query).map(item => item.item)
+		return resourcesFound
 	}
 
 	return { setFilters, filterResources }
