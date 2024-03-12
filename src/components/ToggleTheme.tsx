c0a13901dc1a182454deff0a5fc7cb7bccd97600
Eber <doneber98@gmail.com>
2024-03-12 19:41:42 -0400

    feat: 🎨 improve styles

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -17,7 +17,7 @@ useEffect(() => {
 }, [theme]);
 
 return (
-	<button onClick={handleClick}>
+	<button onClick={handleClick} title="Cambiar tema">
 		<svg width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 496" data-astro-cid-oemx5le4=""><path fill="currentColor" d="M8,256C8,393,119,504,256,504S504,393,504,256,393,8,256,8,8,119,8,256ZM256,440V72a184,184,0,0,1,0,368Z" transform="translate(-8 -8)" data-astro-cid-oemx5le4=""></path></svg>
 	</button>
 );
