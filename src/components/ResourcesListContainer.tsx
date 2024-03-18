8caefc6126cb283414050ecec6a2594f322a123b
Steve Moya Cepeda <100615052@est.uasd.edu.do>
2024-03-18 16:34:09 -0400

    fix: :lipstick: Correcion de css con las cards

--- src/components/ResourcesListContainer.tsx
+++ src/components/ResourcesListContainer.tsx
@@ -5,14 +5,15 @@ export const ResourcesContainer = () => {
   const { resources } = useResources()
 
   return (
-    <ul role="list" class="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3">
+    <ul role="list" class="grid grid-cols-1 gap-3 ">
       {resources.map((resource) => (
         <Card
           href={resource.url}
           title={resource.title}
           description={resource.description}
-          // imageUrl={resource.imageUrl}
+          imageUrl={resource.imageUrl}
           hashtags={resource.hashtags}
+		  
         />
       ))}
     </ul>
--- src/components/searcher/SearchResults.tsx
+++ src/components/searcher/SearchResults.tsx
@@ -11,7 +11,7 @@ export const SearchResults = () => {
 
     <div>
       <h4 className="my-5 text-base">Resultados:</h4>
-      <ul className="flex flex-col gap-4 p-0 my-4">
+		  <ul className="grid grid-cols-1  gap-y-10 gap-x-6 items-start">
         {
           resourcesFiltered.length === 0 ? <p>No se encontraron coincidencias</p> : resourcesFiltered.map((resource) => (
             <Card
