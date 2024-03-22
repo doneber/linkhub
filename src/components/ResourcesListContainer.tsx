03d2b698d8e1c929dc5c755a1c5f8bc14f1118d9
Eber <doneber98@gmail.com>
2024-03-22 12:14:31 -0400

    refactor: ♻️ modularize resource list components

--- src/components/ResourcesListContainer.tsx
+++ src/components/ResourcesListContainer.tsx
@@ -1,7 +1,7 @@
 import type { Resource } from "@src/interfaces/resource.interface"
 import { fetchResources } from "@src/services/resources"
 import { useEffect, useState } from "preact/hooks"
-import { Card } from "./card/Card"
+import { ResourcesList } from "./ResourcesList"
 
 export const ResourcesContainer = () => {
 	const [isLoading, setIsloading] = useState(false)
@@ -32,27 +32,6 @@ export const ResourcesContainer = () => {
 	}, [])
 
 	return (
-		<>
-			<ul role="list" class="grid grid-cols-1 gap-3 ">
-				{resources.map((resource) => (
-					<Card
-						href={resource.url}
-						title={resource.title}
-						description={resource.description ?? ""}
-						imageUrl={resource.imageUrl}
-						hashtags={resource.hashtags}
-
-					/>
-				))}
-			</ul>
-			{
-				isLoading && <div className="text-base text-center">
-					Cargando...
-				</div>
-			}
-			<div className="flex justify-center my-4">
-				<button onClick={handleNextPagination} className="py-2 px-4 rounded border-[1px] border-[solid] border-neutral-700 hover:bg-neutral-800 hover:bg-[0] dark:hover:bg-neutral-100 dark:border-neutral-200 ">Ver mas</button>
-			</div>
-		</>
+		<ResourcesList resources={resources} isLoading={isLoading} handleNextPagination={handleNextPagination} />
 	)
 }
