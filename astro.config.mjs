aef820e09175247ef5ce9bc64db5e95cfde95a1b
Eber <doneber98@gmail.com>
2024-03-28 15:55:54 -0400

    feat: ✨ add authentication

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
-import { defineConfig } from 'astro/config';
-import preact from "@astrojs/preact";
-import tailwind from "@astrojs/tailwind";
-import devtoolbarTailwind from "astro-devtoolbar-tailwind";
+import { defineConfig } from "astro/config"
+import preact from "@astrojs/preact"
+import tailwind from "@astrojs/tailwind"
+import devtoolbarTailwind from "astro-devtoolbar-tailwind"
+import vercel from "@astrojs/vercel/serverless"
 
-import vercel from "@astrojs/vercel/serverless";
+import auth from "auth-astro"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://linkhub.doneber.dev',
-  integrations: [preact(), tailwind(), devtoolbarTailwind()],
+  site: "https://linkhub.doneber.dev",
+  integrations: [preact(), tailwind(), devtoolbarTailwind(), auth()],
   output: "server",
   adapter: vercel()
-});
+})
